52032121c7cfefe1a79b63ac9ed75d246ee4e2d5
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2026-04-26 13:47:31 -0300

    :hammer: db: add initial users migration

---
+++ src/backend/internal/db/migrations/000001_create_users.up.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE USERS (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT,
+    name TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
