1746bdeba4ed3960eaa959837601be78383e95ac
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2026-04-27 21:24:08 -0300

    :sparkles: feat: add Register User implemtation

--- src/backend/internal/auth/service.go
+++ src/backend/internal/auth/service.go
@@ -2,9 +2,15 @@ package auth
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/ottermq/otterboard/src/backend/internal/db"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -27,6 +33,14 @@ type AuthService struct {
 	jwtSecret string
 }
 
+type User struct {
+	ID        string
+	Email     string
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
 func NewAuthService(store UserStore, jwtSecret string) *AuthService {
 	return &AuthService{
 		store:     store,
@@ -35,5 +49,58 @@ func NewAuthService(store UserStore, jwtSecret string) *AuthService {
 }
 
 func (s *AuthService) Register(ctx context.Context, input RegisterInput) (string, error) {
-	panic("not implemented")
+	_, err := s.store.GetUserByEmail(ctx, input.Email)
+	if err == nil {
+		return "", ErrEmailAlreadyExists
+	}
+	if !errors.Is(err, pgx.ErrNoRows) {
+		return "", err
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	user, err := s.store.CreateUser(ctx, db.CreateUserParams{
+		Name:         input.Name,
+		Email:        input.Email,
+		PasswordHash: pgtype.Text{String: string(hash), Valid: true},
+	})
+
+	if err != nil {
+		return "", err
+	}
+	mappedUser := mapDbUserToAuth(user)
+
+	return s.generateAccessToken(mappedUser)
+
+}
+
+func (s *AuthService) generateAccessToken(user User) (string, error) {
+	userJson, err := json.Marshal(user)
+	if err != nil {
+		return "", err
+	}
+
+	claims := jwt.MapClaims{
+		"sub":   user.ID,
+		"email": user.Email,
+		"name":  user.Name,
+		"user":  string(userJson),
+		"exp":   time.Now().Add(24 * time.Hour).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(s.jwtSecret))
+}
+
+func mapDbUserToAuth(user db.User) User {
+	mappedUser := User{
+		ID:        user.ID.String(),
+		Email:     user.Email,
+		Name:      user.Name,
+		CreatedAt: user.CreatedAt.Time,
+		UpdatedAt: user.UpdatedAt.Time,
+	}
+	return mappedUser
 }
